571357bedc6a994c7dffef523adccd264a29e85b
Daniel <146906452+Daniel-Dami@users.noreply.github.com>
2025-12-06 19:24:52 +0800

    updata

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+const imgs = ["image/beach.jpg", "image/castle.jfif", "image/boatoff.png"];
+const links = ["#", "#", "game/boatoff"];
 document.addEventListener("DOMContentLoaded", function () {
-
+    let cnt = 0;
+    setInterval(function () {
+        document.getElementById("slide").src = imgs[cnt];
+        document.getElementById("slided").src = links[cnt];
+        cnt = (cnt + 1) % imgs.length;
+    }, 3000);
 });
