f4a1d9be679aaddd21ab453c33b7277920f37ac5
Daniel <146906452+Daniel-Dami@users.noreply.github.com>
2025-12-06 19:42:58 +0800

    slideshow

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const imgs = ["image/beach.jpg", "image/castle.jfif", "image/boatoff.png"];
+const imgs = ["image/beach.png", "image/castle.png", "image/boatoff.png"];
 const links = ["#", "#", "game/boatoff"];
 document.addEventListener("DOMContentLoaded", function () {
     let cnt = 0;
     setInterval(function () {
         document.getElementById("slide").src = imgs[cnt];
-        document.getElementById("slided").src = links[cnt];
+        document.getElementById("slider").src = links[cnt];
         cnt = (cnt + 1) % imgs.length;
-    }, 3000);
+    }, 1000);
 });
