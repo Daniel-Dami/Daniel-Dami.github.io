1bbdd95f0aae0b9c47ebfddf0f5c1562a52dc877
Daniel <146906452+Daniel-Dami@users.noreply.github.com>
2026-04-25 18:54:21 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const imgs = ["image/beach.png", "image/castle.png", "image/boatoff.png"];
 const links = ["#", "#", "game/boatoff"];
+let cnt = 0;
 function changeSlide() {
     document.getElementById("slide").src = imgs[cnt];
     // document.getElementById("slit").src = links[cnt];
     cnt = (cnt + 1) % imgs.length;
 }
 document.addEventListener("DOMContentLoaded", function () {
-    let cnt = 0;
     setInterval(changeSlide, 1000);
 });
